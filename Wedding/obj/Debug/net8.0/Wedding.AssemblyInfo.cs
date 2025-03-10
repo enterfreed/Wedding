dee9000c1d1274f3ca4d686bf49322cc4f861a86
senichkin.aa <senichkin.aa@glonasssoft.ru>
2025-03-10 07:52:00 +0300

    Что то поделали еще

--- Wedding/obj/Debug/net8.0/Wedding.AssemblyInfo.cs
+++ Wedding/obj/Debug/net8.0/Wedding.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wedding")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+008b1f69f3987d0751bac2b85b6083952a36728a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+253cb075c8d0a1d8e246b457e28b6f029b5115d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wedding")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wedding")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
